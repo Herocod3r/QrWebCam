412e7f84c6a8d7de0450619a94979d35c2d70a75
Marcos Vinicius Maia <juninhowblu@gmail.com>
2017-03-28 20:47:36 -0300

    Improvements V1
    Massively improved CPU usage.
    Fixed memory leak, altough not entirely.
    Added StatusText property to allow localization.
    Code reformat and cleanup

--- QrWebCam/CameraDevices.cs
+++ QrWebCam/CameraDevices.cs
@@ -1,69 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using AForge.Video.DirectShow;
 
 namespace QrWebCam
 {
-    using AForge.Video.DirectShow;
-    using System;
-    using System.Collections.Generic;
-    using System.ComponentModel;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-   
-        public class CameraDevices : INotifyPropertyChanged
+    public class CameraDevices : INotifyPropertyChanged
+    {
+        public CameraDevices()
         {
-            public CameraDevices()
-            {
-                GatherDevices();
-            }
+            GatherDevices();
+        }
 
-            public void GatherDevices() => devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+        public void GatherDevices() => devices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
 
-            private FilterInfoCollection devices;
-            public VideoCaptureDevice Current { get; set; }
-            public FilterInfoCollection Devices
-            {
-                get
-                {
-                    return devices;
-                }
-                set
-                {
-                    devices = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Devices"));
-                }
-            }
-            /// <summary>
-            /// Gets all camera devices names from this system
-            /// </summary>
-            /// <returns></returns>
-            public List<string> GetStringCameras()
-            {
-                if (devices.Count <= 0) return new List<string>();
-                List<string> dvs = new List<string>();
-                for (int i = 0; i < devices.Count; i++)
-                {
-                    dvs.Add(Devices[i].Name);
-                }
-                return dvs;
-            }
+        private FilterInfoCollection devices;
+        public VideoCaptureDevice Current { get; set; }
 
-            public event PropertyChangedEventHandler PropertyChanged;
-
-            /// <summary>
-            /// Activates a camera based on returned index from the GetStringCameras
-            /// </summary>
-            /// <param name="index"></param>
-            public void SelectCamera(int index)
+        public FilterInfoCollection Devices
+        {
+            get { return devices; }
+            set
             {
-                if (index >= Devices.Count) throw new ArgumentOutOfRangeException("Index is illegal");
-                Current = new VideoCaptureDevice(Devices[index].MonikerString);
+                devices = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Devices"));
             }
         }
 
+        /// <summary>
+        ///     Gets all camera devices names from this system
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetStringCameras()
+        {
+            if (devices.Count <= 0) return new List<string>();
+            var dvs = new List<string>();
+            for (var i = 0; i < devices.Count; i++)
+            {
+                dvs.Add(Devices[i].Name);
+            }
+            return dvs;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        ///     Activates a camera based on returned index from the GetStringCameras
+        /// </summary>
+        /// <param name="index"></param>
+        public void SelectCamera(int index)
+        {
+            if (index >= Devices.Count) throw new ArgumentOutOfRangeException("Index is illegal");
+            Current = new VideoCaptureDevice(Devices[index].MonikerString);
+        }
+    }
 }
